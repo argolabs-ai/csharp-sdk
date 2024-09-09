fdba6532f6d05e8bae66126e77ec00582e121133
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-09-09 12:36:46 +0000

    SDK regeneration

--- src/ArgoLabs.net/Core/Public/ArgoLabsEnvironment.cs
+++ src/ArgoLabs.net/Core/Public/ArgoLabsEnvironment.cs
@@ -2,5 +2,5 @@ namespace ArgoLabs.net;
 
 public class ArgoLabsEnvironment
 {
-    public static string Staging = "https://dev-api-dev.up.railway.app";
+    public static string Staging = "https://api.argolabs.ai";
 }
